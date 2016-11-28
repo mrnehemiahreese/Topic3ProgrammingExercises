298c7db70328095841b122b223d78470b1441562
mrnehemiahreese <nehemiahreese@gmail.com>
2016-11-27 20:36:21 -0600

    Add files via upload

--- Exercise4p23.java
+++ Exercise4p23.java
@@ -1,4 +1,25 @@
-
+import java.util.Scanner;
 public class Exercise4p23 {
-
+	public static void main(String[] args) {
+		
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter employee's name: ");
+		String name = input.nextLine();
+		System.out.println("Enter number of hours worked in a week: ");
+		double hours = input.nextDouble();
+		System.out.println("Enter hourly pay rate: ");
+		double payrate = input.nextDouble();
+		System.out.println("Enter federal tax withholding rate: ");
+		double fedtax = input.nextDouble();
+		System.out.println("Enter state tax withholding rate: ");
+		double statetax = input.nextDouble();
+		
+		
+		
+		
+		 
+	}
 }
